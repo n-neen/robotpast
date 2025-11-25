b0345a40616d3478080c02372dc238531e887569
n-neen <rathergoodyogurt@gmail.com>
2025-11-24 23:15:26 -0500

    mockups

--- newgame.bat
+++ newgame.bat
@@ -1,3 +1,3 @@
-"E:\Program Files (x86)\Emulators\super metroid\asar\asar.exe" --no-title-check -v C:\Users\keenan\Desktop\robotpast_repo\robotpast\robotpast.asm C:\Users\keenan\Desktop\robotpast_repo\robotpast\robotpast.sfc
+"E:\Program Files (x86)\Emulators\super metroid\asar\asar.exe" --no-title-check -v C:\Users\keenan\Desktop\newgame_repo\robotpast\robotpast.asm C:\Users\keenan\Desktop\newgame_repo\robotpast\robotpast.sfc
 
 pause
